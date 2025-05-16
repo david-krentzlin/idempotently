c46a0b0092c9ba8395faa6e97f43e26f0277b972
David Krentzlin <david.krentzlin@new-work.se>
2025-05-16 16:29:41 +0200

    remove needless comments

--- lib/idempotently/version.rb
+++ lib/idempotently/version.rb
@@ -1,30 +1,5 @@
 # frozen_string_literal: true
 
 module Idempotently
-  VERSION = "0.1.0"
+  VERSION = '0.1.0'
 end
-
-# class Processor < Xing::AMQP::Processor
-#   include Idempotently
-#
-#   def process
-#     idempotently(message.id, executor: :messaging) do
-#       deliver_email
-#     end
-#   end
-# end
-
-# class MailController < ApplicationController
-#   def send_email
-#     result = idempotently(request.headers[:idempotency_key], executor: :mail) do
-#       deliver_email(params[:email])
-#     end
-#   end
-# end
-#
-# Idempotently.configure do |config|
-#   config.executor(:messaging) do |msg_config|
-#     msg_config.storage = Idempotently::Storage::RedisAdapter.new(ENV['REDIS_URL'])
-#     msh_config.window = 30.minutes
-#   end
-# end
